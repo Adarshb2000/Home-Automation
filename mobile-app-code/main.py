62d8c45cd9351c36e234978b338eb4432dc2b9c4
Adarsh <adarshb20@outlook.com>
2020-10-23 18:43:02 +0530

    added changes

--- mobile-app-code/main.py
+++ mobile-app-code/main.py
@@ -4,19 +4,21 @@ from kivymd.uix.boxlayout import MDBoxLayout
 from kivymd.uix.gridlayout import MDGridLayout
 from kivymd.uix.floatlayout import MDFloatLayout
 from kivymd.uix.navigationdrawer import NavigationLayout, MDNavigationDrawer, MDToolbar
-from kivymd.uix.card import MDCard
-from kivy.uix.switch import Switch
+from kivymd.uix.card import MDCard, MDSeparator
 from kivymd.uix.screen import Screen
-from kivy.uix.screenmanager import ScreenManager
 from kivymd.uix.button import Button, MDFlatButton, MDFloatingActionButton, MDIconButton
-from kivy.uix.image import Image
 from kivymd.uix.slider import MDSlider
-from kivymd.toast import toast
 from kivymd.uix.selectioncontrol import MDSwitch
+from kivymd.toast import toast
 from kivymd.uix.label import MDLabel
+from kivy.uix.switch import Switch
+from kivy.uix.screenmanager import ScreenManager
+from kivy.uix.image import Image
 from kivy.config import Config
+from kivy.uix.popup import Popup
 from functools import partial
 import paho.mqtt.client as mqtt
+from time import time
 
 Config.set('graphics', 'resizable', True)
 
@@ -24,6 +26,10 @@ Config.set('graphics', 'resizable', True)
 # Variables
 username = "root_system0"
 password = "random.random()"
+
+
+class cardButton(MDCard, Button):
+    pass
 
 
 class Room(MDBoxLayout):
@@ -75,25 +81,90 @@ class Room(MDBoxLayout):
 """
 
         # AC region
+        self.timer = 0
         new_layout = MDFloatLayout()
-        new_card = MDCard(
+        self.new_card = MDCard(
+            orientation='vertical',
+            padding='10dp',
+            pos_hint={'center_x': 0.5, 'center_y': 0.5},
+            size_hint=(0.75, 0.75),
+        )
+        self.new_card.md_bg_color = app.off_red
+        ac_image = Image(
+            source='static/ac1.png',
+        )
+        ac_image.size_hint_y = 0.9
+        
+        ac_image.pos_hint = {'top': 1.0, 'center_x' : 0.5}
+        self.new_card.radius = [4] * 4
+
+        # ac_button.bind(active=callback)
+        self.new_card.add_widget(ac_image)
+        self.new_card.on_press = self.ac_touch_down
+        self.new_card.on_release = self.ac_touch_up
+        temp = MDLabel(text='18ºC')
+        temp.font_size = 10
+        temp.size = (0.25, 1)
+        temp.color = [1, 1, 1, 1]
+        new_layout.add_widget(self.new_card)
+        screen_layout.add_widget(new_layout)
+
+        self.ac_popup = Popup(
+            title = 'AC settings',
+            size_hint = (.75, .75)
+        )
+        self.ac_popup.background_color = [i / 255 for i in [137, 205, 211]] + [1]
+        popup_layout = MDGridLayout()
+
+        # AC image
+        self.image_box = MDBoxLayout(orientation='vertical')
+        popup_layout.cols = 1
+        ac_image1 = Image(
+            source='static/ac1.png',
+            size_hint=(0.9, 0.9),
+        )
+        ac_image1.pos_hint = {'top' : 1}
+        self.image_box.size_hint = (0.5, 0.5)
+        self.image_box.add_widget(ac_image1)
+        self.image_box.add_widget(MDLabel(text='18ºC'))
+        popup_layout.add_widget(self.image_box)
+        self.ac_popup.on_open = self.faltu
+
+
+        # all the features for ac
+        features_layout = MDGridLayout()
+        features_layout.cols = 2
+
+        # Temp incease, decrease
+        new_box_layout = MDBoxLayout(orientation='vertical')
+        inc_button = MDCard(
             orientation='horizontal',
             padding='10dp',
             pos_hint={'center_x': 0.5, 'center_y': 0.5},
             size_hint=(0.75, 0.75),
         )
-        new_card.md_bg_color = [i / 255 for i in [220, 20, 60]] + [1]
-        ac_image = Image(
-            source='static/ac1.png',
-            size_hint=(0.9, 0.9),
+        inc_button.md_bg_color = [i / 255 for i in [246, 205, 139]] + [1]
+        inc_button.add_widget(MDLabel(text='Temp+'))
+        dec_button = MDCard(
+            orientation='horizontal',
+            padding='10dp',
+            pos_hint={'center_x': 0.5, 'center_y': 0.5},
+            size_hint=(0.75, 0.75),
         )
-        ac_image.pos_hint = {'top': 1.0}
-        new_card.radius = [5] * 4
+        dec_button.md_bg_color = [i / 255 for i in [136, 199, 220]] + [1]
+        dec_button.add_widget(MDLabel(text='Temp-'))
+        features_layout.add_widget(inc_button)
+        features_layout.add_widget(dec_button)
 
-        # ac_button.bind(active=callback)
-        new_card.add_widget(ac_image)
-        new_layout.add_widget(new_card)
-        screen_layout.add_widget(new_layout)
+
+        #-----------------#
+        features_layout.add_widget(Button(text='TODO'))
+        features_layout.add_widget(Button(text='TODO'))
+        features_layout.add_widget(Button(text='TODO'))
+        features_layout.add_widget(Button(text='TODO'))
+        
+        popup_layout.add_widget(features_layout)
+        self.ac_popup.content = popup_layout
 
         #--------------------------------------------------------------#
 
@@ -111,7 +182,7 @@ class Room(MDBoxLayout):
             source='static/light1.png',
             size_hint=(1, 1),
         )
-        self.light_card.on_touch_down = self.light_change
+        self.light_card.on_press = self.light_change
         self.light_card.add_widget(light_image)
         new_layout.add_widget(self.light_card)
         screen_layout.add_widget(new_layout)
@@ -126,8 +197,20 @@ class Room(MDBoxLayout):
         screen_layout.add_widget(Button(text='TODO'))
         self.add_widget(screen_layout)
 
-    def timer_popup(self, *args):
-        pass
+    def faltu(self, *args):
+        self.image_box.md_bg_color = self.new_card.md_bg_color
+
+    def ac_touch_down(self, *args):
+        self.timer = time()
+        
+    
+    def ac_touch_up(self, *args):
+        if (time() - self.timer > 0.5):
+            self.ac_popup.open()
+        else:
+            app.switch(0, 'ac', 1)
+            self.new_card.md_bg_color = app.on_green if self.new_card.md_bg_color == app.off_red else app.off_red 
+        self.timer = 0
 
     def button_change(self, btn, *args):
         if btn.background_color == [0, 0, 0, 1]:
@@ -189,6 +272,8 @@ class RoomApp(MDApp):
         # Colors
         self.dark_color = [i / 255 for i in [50] * 3] + [1]
         self.light_color = [i / 255 for i in [240, 240, 240]] + [1]
+        self.on_green = [i / 255 for i in [46, 139, 87]] + [1]
+        self.off_red = [i / 255 for i in [220, 20, 60]] + [0.99]
 
 
         # Connection to home server
@@ -196,7 +281,7 @@ class RoomApp(MDApp):
         self.client.username_pw_set(username, password)
 
         self.client.connect('192.168.29.148', 1883, 60)
-        self.client.publish('test', 'test')
+        self.client.publish('test', '1, 1')
         
         
 
